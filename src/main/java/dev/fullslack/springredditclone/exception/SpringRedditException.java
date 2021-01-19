fb4d7bd6fa75f5b27f0fb6db30e29b8edce7d3e6
Bart L <loeffen@hotmail.com>
2021-01-19 16:06:08 +0100

    Part 2 completed

---
+++ src/main/java/dev/fullslack/springredditclone/exception/SpringRedditException.java
@@ -0,0 +1,11 @@
+package dev.fullslack.springredditclone.exception;
+
+public class SpringRedditException extends RuntimeException {
+    public SpringRedditException(String exMessage, Exception exception) {
+        super(exMessage, exception);
+    }
+
+    public SpringRedditException(String exMessage) {
+        super(exMessage);
+    }
+}
---
+++ src/main/java/dev/fullslack/springredditclone/service/AuthService.java
@@ -0,0 +1,53 @@
+package dev.fullslack.springredditclone.service;
+
+import dev.fullslack.springredditclone.dto.RegisterRequest;
+import dev.fullslack.springredditclone.model.NotificationEmail;
+import dev.fullslack.springredditclone.model.User;
+import dev.fullslack.springredditclone.model.VerificationToken;
+import dev.fullslack.springredditclone.repository.UserRepository;
+import dev.fullslack.springredditclone.repository.VerificationTokenRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@Service
+@AllArgsConstructor
+public class AuthService {
+
+    private final PasswordEncoder passwordEncoder;
+    private final UserRepository userRepository;
+    private final VerificationTokenRepository verificationTokenRepository;
+    private final MailService mailService;
+
+    @Transactional
+    public void signup(RegisterRequest registerRequest) {
+        User user = new User();
+        user.setUsername(registerRequest.getUsername());
+        user.setPassword(passwordEncoder.encode(registerRequest.getPassword()));
+        user.setEmail(registerRequest.getEmail());
+        user.setCreatedDate(Instant.now());
+        user.setEnabled(false);
+
+        userRepository.save(user);
+        
+        String token = generateVerificationToken(user);
+        mailService.sendMail(new NotificationEmail("Please Activate your Account",
+                user.getEmail(), "Thank you for signing up to Spring Reddit, " +
+                "please click on the below url to activate your account: " +
+                "http://localhost:8080/api/auth/accountVerification/" + token));
+    }
+
+    private String generateVerificationToken(User user) {
+        String token = UUID.randomUUID().toString();
+        VerificationToken verificationToken = new VerificationToken();
+        verificationToken.setToken(token);
+        verificationToken.setUser(user);
+
+        verificationTokenRepository.save(verificationToken);
+        return token;
+    }
+}
