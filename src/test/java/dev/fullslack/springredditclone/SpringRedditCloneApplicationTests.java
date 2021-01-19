06e6414fa9927fe881cd12448959b1280697246f
Bart L <loeffen@hotmail.com>
2021-01-19 13:34:54 +0100

    Initial commit

---
+++ src/test/java/dev/fullslack/springredditclone/SpringRedditCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.fullslack.springredditclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRedditCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
